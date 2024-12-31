bc40c76227c836b942b27f1a4e410ac5d1ddf26e
Wushengyouhan <840835067@qq.com>
2024-12-31 11:59:44 +0800

    337. Finalizing the Fetching Logic

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -18,8 +18,13 @@ export const fetchCartData = () => {
     };
 
     try {
-      const cartdata = await fetchData();
-      dispatch(cartActions.replaceCart(cartdata));
+      const cartData = await fetchData();
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -47,7 +52,10 @@ export const sendCartData = (cart) => {
         "https://cart-b5793-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
 
